b639856e6cb92821f4f90b8b9b0741f8e7ba4bd5
Kaique Rocha <kaiquerocha@MacBook-Air-de-Kaique.local>
2024-05-19 12:24:11 -0300

    feat: validando campos

--- src/models/User.go
+++ src/models/User.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // User representa um usuário utilizando a rede social
 type User struct {
@@ -10,4 +14,39 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"senha,omitempty"`
 	CreatedAt time.Time `json:"criado_em,omitempty"`
+}
+
+func (user *User) Prepare() error {
+	if error := user.validate(); error != nil {
+		return error
+	}
+
+	user.format()
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("O nome é obrigatorio e não pode estar em branco")
+	}
+
+	if user.Nick == "" {
+		return errors.New("O nick é obrigatorio e não pode estar em branco")
+	}
+
+	if user.Email == "" {
+		return errors.New("O e-mail é obrigatorio e não pode estar em branco")
+	}
+
+	if user.Password == "" {
+		return errors.New("A senha é obrigatorio e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Email = strings.TrimSpace(user.Email)
+	user.Nick = strings.TrimSpace(user.Nick)
 }
